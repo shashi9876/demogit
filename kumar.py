9f1c388a82bf346e9573e00e5d939ca552bd1bfb
shashi9876 <shashi.shashikumar.kumar4241@gmail.com>
2022-06-30 18:25:07 +0530

    socket added

--- kumar.py
+++ kumar.py
@@ -1,3 +1,17 @@
-print("Hello Kumar")
-print("How are you")
-print("Its been a long time since we had met earlier")
+import socket
+
+s=socket.socket()
+print('socket created')
+s.bind(('localhost',9999))
+s.listen(3)
+print('Waiting for connections')
+
+while True:
+    c,addr=s.accept()
+    name=c.recv(1024).decode()
+    print('connected with ',addr,name)
+    c.send(bytes('welcome to Myworld','utf-8'))
+
+    c.close()
+
+
